74798739ca88e13d3e7a689d826a95c87fa42d0d
terzi-vladislav <terzi.va@phystech.edu>
2020-04-24 05:25:36 +0300

    Animation for buttons added, tap recognition, resizable interface

---
+++ script.js
@@ -0,0 +1,10 @@
+let mac_button = document.getElementById("mac_download");
+let win_button = document.getElementById("win_download");
+
+mac_button.onclick = () => {
+    console.log("mac")
+}
+
+win_button.onclick = () => {
+    console.log("win")
+}
