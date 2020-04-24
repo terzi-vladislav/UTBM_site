1fb0220a8238758f55889fbd19b45f93d7b5536f
terzi-vladislav <terzi.va@phystech.edu>
2020-04-24 06:34:28 +0300

    Added versions for download

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 let mac_button = document.getElementById("mac_download");
 let win_button = document.getElementById("win_download");
 
+let modal = document.getElementById('mac_instructions');
+modal.style.display = "none";
+
 mac_button.onclick = () => {
-    console.log("mac")
+    show_mac_manual();
+    download("MacOSBuild.zip");
 }
 
 win_button.onclick = () => {
-    console.log("win")
+    download("WindowsBuild.zip");
+}
+
+function show_mac_manual() {
+    let modal = document.getElementById('mac_instructions');
+    modal.style.display = "block";
+
+    let span = document.getElementsByClassName("close")[0];
+
+    span.onclick = () => {
+        modal.style.display = "none";
+    }
+}
+
+function download(filename) {
+    let element = document.createElement('a');
+    if (filename == "MacOSBuild.zip") {
+        element.setAttribute('href', 'Versions/MacOSBuild.zip');
+    } else {
+        element.setAttribute('href', 'Versions/WindowsBuild.zip');
+    }
+
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
