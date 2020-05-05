e89c1f8ef6a67b1b065765736f32d14451d9e490
terzi-vladislav <terzi.va@phystech.edu>
2020-05-06 02:53:09 +0300

    Update

---
+++ public/init-firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyC5EyhBD_u2RkMqM7Qsd8vghf6wB2qrAN0",
+    authDomain: "under-the-blue-moon.firebaseapp.com",
+    databaseURL: "https://under-the-blue-moon.firebaseio.com",
+    projectId: "under-the-blue-moon",
+    storageBucket: "under-the-blue-moon.appspot.com",
+    messagingSenderId: "56196221133",
+    appId: "1:56196221133:web:8c4d57c209087c6b4ec184",
+    measurementId: "G-MBTE8387MH"
+};
+
+firebase.initializeApp(firebaseConfig);
--- public/script.js
+++ public/script.js
@@ -7,10 +7,12 @@ modal.style.display = "none";
 mac_button.onclick = () => {
     show_mac_manual();
     download("MacOSBuild.zip");
+    firebase.analytics().logEvent('Download_for_mac');
 }
 
 win_button.onclick = () => {
     download("WindowsBuild.zip");
+    firebase.analytics().logEvent('Download_for_windows');
 }
 
 function show_mac_manual() {
@@ -47,15 +49,15 @@ function writeUserData(email) {
         email: email
     };
 
-    let emailValid = document.getElementById("subscribe")
-
     firebase.database().ref('subscription-entries').push(emailObject)
         .then(() => {
             let email = document.getElementById("inputEmail");
             email.value = "";
-            console.log('success');
+            show_success();
         }, function(error) {
-            console.log('error' + error);
+            let email = document.getElementById("inputEmail");
+            email.value = "";
+            email.placeholder = error;
         });
 }
 
@@ -68,7 +70,69 @@ subscribeButton.addEventListener('click',
         if (!error) {
             writeUserData(email);
         } else {
-            console.log(error);
+            let email = document.getElementById("inputEmail");
+            email.value = "";
+            email.placeholder = error;
         }
     });
 
+let modal1 = document.getElementById('success');
+modal1.style.display = "none";
+
+function show_success() {
+    let modal = document.getElementById('success');
+    modal.style.display = "flex";
+
+    let span = document.getElementById('success_close');
+
+    span.onclick = () => {
+        modal.style.display = "none";
+    }
+}
+
+let report_button = document.getElementById("bug_button");
+
+let modal3 = document.getElementById('report');
+modal3.style.display = "none";
+
+report_button.onclick = () => {
+    show_bug_report();
+}
+
+function show_bug_report() {
+    let modal = document.getElementById('report');
+    modal.style.display = "flex";
+
+    let span = document.getElementById('report_close');
+
+    span.onclick = () => {
+        modal.style.display = "none";
+    }
+}
+
+function report(text) {
+    let reportObject = {
+        report: text
+    };
+
+    firebase.database().ref('reports').push(reportObject)
+        .then(() => {
+            let report = document.getElementById('problem');
+            report.value = "";
+            let modal = document.getElementById('report');
+            modal.style.display = "none";
+            show_success();
+        }, function(error) {
+            let report = document.getElementById('report');
+            report.value = "";
+            report.placeholder = error;
+        });
+}
+
+let issueButton = document.getElementById('report_issue');
+
+issueButton.addEventListener('click',
+    () => {
+        let text = document.getElementById('problem').value;
+        report(text);
+    });
